581683a90d2625552b01bfbbe0d354ece185726c
Colton Randall <coltonrandall.nz@gmail.com>
2025-04-01 18:45:50 +1300

    create MovieCard component

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,17 @@
 import "./App.css";
+import React from "react";
+import MovieCard from "./components/MovieCard";
 
 export default function App() {
-  return <></>;
+  const movieNumber = 2;
+
+  return (
+    <>
+      {movieNumber == 1 ? (
+        <MovieCard key={1} title="Movie1" releaseDate="2023" />
+      ) : (
+        <MovieCard key={2} title="Movie 2" releaseDate="2023" />
+      )}
+    </>
+  );
 }
---
+++ src/components/MovieCard.jsx
@@ -0,0 +1,24 @@
+function MovieCard({ title, url, releaseDate }) {
+  function onFavouriteClick() {
+    alert("clicked");
+  }
+
+  return (
+    <div className="movie-card">
+      <div className="movie-poster">
+        <img src={url} alt={title} />
+        <div className="movie-overlay">
+          <button className="favourite-btn" onClick={onFavouriteClick}>
+            ❤️
+          </button>
+        </div>
+      </div>
+      <div className="movie-info">
+        <h3>{title}</h3>
+        <p>{releaseDate}</p>
+      </div>
+    </div>
+  );
+}
+
+export default MovieCard;
